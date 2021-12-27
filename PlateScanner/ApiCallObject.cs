7b618dbc8570f0af7bd0713268e999227828aa47
Randel M <Randel.C.Moore@gmail.com>
2021-12-27 13:58:05 -0800

    Adds URLencoding example and SQL query examples

--- PlateScanner/ApiCallObject.cs
+++ PlateScanner/ApiCallObject.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using RestSharp;
 using RestSharp.Authenticators;
+using RestSharp.Extensions;
+using System.Web;
 
 namespace PlateScanner
 {
@@ -34,7 +36,7 @@ namespace PlateScanner
         {
 
             var client = new RestClient();
-            var request = new RestRequest(this.Url);
+            var request = new RestRequest(this.Url + UrlParameters);
 
             //request.AddQueryParameter("UrlParameters", this.UrlParameters);
 
@@ -57,6 +59,9 @@ namespace PlateScanner
                 this.ApiResponse = response.Content;
             }
 
+            string myUrlString = "SELECT plate, ObjId, s.cx, s.cy FROM PhotoObj AS p JOIN SpecObj AS s ON s.bestobjid = p.objid WHERE s.plate <= 2534 AND s.plate >= 2533";
+            string myEncodedString = HttpUtility.UrlEncode(myUrlString);
+            Console.WriteLine(myEncodedString);
             Console.WriteLine(this.ApiResponse);
 
         }
@@ -64,3 +69,64 @@ namespace PlateScanner
        
     }
 }
+
+// Example Search - https://skyserver.sdss.org/dr16/SkyServerWS/SearchTools/SqlSearch?cmd=select%20top%2010%20ra,dec%20from%20Frame&format=json
+
+// URL encoding
+
+// '+' or '%02' is ' ' (space)
+// '%3D' is '=' (equals)
+// '%2C' is ',' (equals)
+// '<' or '%3c' is '<' (less than)
+// '>' or '%3e' is '>' (greater than)
+// '%0D%0A' is (enter/return/newline)
+
+// Base URL: http://skyserver.sdss.org/dr17/en/tools/search/x_results.aspx?searchtool=SQL&TaskName=Skyserver.Search.SQL&syntax=NoSyntax&ReturnHtml=true&cmd=
+// SQL Select statement = SELECT plate, ObjId, s.cx, s.cy FROM PhotoObj AS p JOIN SpecObj AS s ON s.bestobjid = p.objid WHERE s.plate <= 2534 AND s.plate >= 2533
+// URL query string = SELECT+plate%2C+ObjId%2C+s.cx%2C+s.cy+FROM+PhotoObj+AS+p+JOIN+SpecObj+AS+s+ON+s.bestobjid+%3D+p.objid+WHERE+s.plate+<%3D+2534+AND+s.plate+>%3D+2533
+// return data format = &format=html OR &format=jsonx
+// full URL = http://skyserver.sdss.org/dr17/en/tools/search/x_results.aspx?searchtool=SQL&TaskName=Skyserver.Search.SQL&syntax=NoSyntax&ReturnHtml=true&cmd=SELECT+plate%2C+ObjId%2C+s.cx%2C+s.cy+FROM+PhotoObj+AS+p+JOIN+SpecObj+AS+s+ON+s.bestobjid+%3D+p.objid+WHERE+s.plate+<%3D+2534+AND+s.plate+>%3D+2533&format=html
+
+// Example API Calls
+
+
+//Finds all objects on a plate?!?!
+//SELECT
+//plate, specObjID
+//FROM 
+//SpecObjAll
+//WHERE
+//plate = 2534
+
+//SELECT
+//*
+//FROM 
+//SpecObjAll
+//WHERE
+//specObjID = 9262920495199967232
+//Order by xFocal asc
+
+
+
+//SELECT
+//plate, ObjId, s.cx, s.cy
+//FROM PhotoObj AS p
+//JOIN SpecObj AS s ON s.bestobjid = p.objid
+//WHERE 
+//s.plate = 2534
+
+
+//SELECT
+//plate, p.ObjId, s.cx, s.cy
+//FROM 
+//PhotoObjAll AS p
+//JOIN 
+//SpecObjAll AS s ON s.specObjID = p.specObjID
+//WHERE 
+//s.plate = 2534
+
+
+//Primary Keys
+//SpecObjAll - primary   specObjID
+//PhotoObjAll - primary - objID
+//Plate2Target - Primary - Plate3TargetID
